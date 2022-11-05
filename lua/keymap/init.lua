beacee49111ce2a00111d8237eb39b5ae3f5bf3d
Marcus Weber <weberma@students.uni-marburg.de>
2022-11-05 14:50:45 +0100

    update: add lsp_reference keymap for telescope

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -19,41 +19,37 @@ local map = vim.keymap.set
 
 -- usage of plugins
 nmap({
-	-- packer
-	{ "<Leader>pu", cmd("PackerUpdate"), opts(noremap, silent) },
-	{ "<Leader>pi", cmd("PackerInstall"), opts(noremap, silent) },
-	{ "<Leader>pc", cmd("PackerCompile"), opts(noremap, silent) },
-	-- dashboard
-	{ "<Leader>n", cmd("DashboardNewFile"), opts(noremap, silent) },
-	{ "<Leader>ss", cmd("SessionSave"), opts(noremap, silent) },
-	{ "<Leader>sl", cmd("SessionLoad"), opts(noremap, silent) },
-	-- neo-tree
-	{ "<Leader>e", cmd("NeoTreeRevealToggle"), opts(noremap, silent) },
-	-- leap
-	{ "s", plug("(leap-forward)"), opts(noremap, silent) },
-	{ "S", plug("(leap-backward)"), opts(noremap, silent) },
-	-- Telescope
-	{ "<Leader>b", cmd("Telescope buffers"), opts(noremap, silent) },
-	{ "<Leader>fg", cmd("Telescope live_grep"), opts(noremap, silent) },
-	{ "<Leader>ff", cmd("Telescope find_files"), opts(noremap, silent) },
-	{ "<Leader><Leader>", cmd("Telescope current_buffer_fuzzy_find"), opts(noremap, silent) },
-	{ "<Leader>gs", cmd("Telescope current_buffer_fuzzy_find"), opts(noremap, silent) },
-	-- Lsp Saga
-	{ "gh", cmd("Lspsaga lsp_finder"), opts(noremap, silent) },
-	{ "<Leader>lr", cmd("Lspsaga rename"), opts(noremap, silent) },
-	{ "<Leader>o", cmd("LSoutlineToggle"), opts(noremap, silent) },
-	{ "K", cmd("Lspsaga hover_doc"), opts(noremap, silent) },
-	{ "<Leader>la", cmd("Lspsaga code_action"), opts(noremap, silent) },
-	{ "<Leader>pd", cmd("Lspsaga peek_definition"), opts(noremap, silent) },
-	{ "<Leader>lF", cmd("Lspsaga show_line_diagnostics"), opts(noremap, silent) },
-	{ "<Leader>lI", cmd("Lspsaga show_cursor_diagnostics"), opts(noremap, silent) },
-	{ "<Leader>9", cmd("Lspsaga diagnostic_jump_next"), opts(noremap, silent) },
-	{ "<Leader>8", cmd("Lspsaga diagnostic_jump_prev"), opts(noremap, silent) },
-  -- Trouble
-	{ "<Leader>xx", cmd("TroubleToggle"), opts(noremap, silent) },
-  -- Toggleterm
-	{ "<C-t>", cmd("ToggleTerm"), opts(noremap, silent) },
+  -- packer
+  { "<Leader>pu", cmd("PackerUpdate"), opts(noremap, silent) },
+  { "<Leader>pi", cmd("PackerInstall"), opts(noremap, silent) },
+  { "<Leader>pc", cmd("PackerCompile"), opts(noremap, silent) },
 
+  -- neo-tree
+  { "<Leader>e", cmd("NeoTreeRevealToggle"), opts(noremap, silent) },
+  -- leap
+  { "s", plug("(leap-forward)"), opts(noremap, silent) },
+  { "S", plug("(leap-backward)"), opts(noremap, silent) },
+  -- Telescope
+  { "<Leader>b", cmd("Telescope buffers"), opts(noremap, silent) },
+  { "<Leader>fg", cmd("Telescope live_grep"), opts(noremap, silent) },
+  { "<Leader>ff", cmd("Telescope find_files"), opts(noremap, silent) },
+  { "<Leader><Leader>", cmd("Telescope current_buffer_fuzzy_find"), opts(noremap, silent) },
+  { "<Leader>gs", cmd("Telescope current_buffer_fuzzy_find"), opts(noremap, silent) },
+  -- Lsp Saga
+  { "gh", cmd("Lspsaga lsp_finder"), opts(noremap, silent) },
+  { "<Leader>lr", cmd("Lspsaga rename"), opts(noremap, silent) },
+  { "<Leader>o", cmd("LSoutlineToggle"), opts(noremap, silent) },
+  { "K", cmd("Lspsaga hover_doc"), opts(noremap, silent) },
+  { "<Leader>la", cmd("Lspsaga code_action"), opts(noremap, silent) },
+  { "<Leader>pd", cmd("Lspsaga peek_definition"), opts(noremap, silent) },
+  { "<Leader>lF", cmd("Lspsaga show_line_diagnostics"), opts(noremap, silent) },
+  { "<Leader>lI", cmd("Lspsaga show_cursor_diagnostics"), opts(noremap, silent) },
+  { "<Leader>9", cmd("Lspsaga diagnostic_jump_next"), opts(noremap, silent) },
+  { "<Leader>8", cmd("Lspsaga diagnostic_jump_prev"), opts(noremap, silent) },
+  -- Trouble
+  { "<Leader>xx", cmd("TroubleToggle"), opts(noremap, silent) },
+  -- Toggleterm
+  { "<C-t>", cmd("ToggleTerm"), opts(noremap, silent) },
 })
 
 tmap({
@@ -61,7 +57,7 @@ tmap({
   { "<C-t>", cmd("ToggleTerm"), opts(noremap, silent) },
 })
 
-
 map("n", "<Leader>gs", require("utils.telescope-commands").git_status)
+map("n", "<Leader>ls", require("telescope.builtin").lsp_document_symbols)
 
--- 
+--
