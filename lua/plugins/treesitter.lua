444049adebd317f34552e11aafff43048376bdfa
Marcus Weber <weberma@students.uni-marburg.de>
2023-04-25 20:14:25 +0200

    add ts rainbow

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,9 +16,30 @@ return {
       })
     end,
     opts = {
+
       multiline_threshold = 4,
       separator = { "─", "ContextBorder" }, -- alternatives: ▁ ─ ▄
       mode = "cursor",
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { "HiPhish/nvim-ts-rainbow2" },
+    opts = {
+      rainbow = {
+        enable = true,
+        disable = false,
+        query = {
+          "rainbow-parens",
+          tsx = function()
+            return nil
+          end,
+          javascript = function()
+            return nil
+          end,
+        },
+      },
     },
   },
 }
