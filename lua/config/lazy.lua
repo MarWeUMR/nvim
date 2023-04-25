c01571d782613726154020d3ec45ffc61794509b
Marcus Weber <weberma@students.uni-marburg.de>
2023-04-25 20:13:35 +0200

    deactivate for now due to performance

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.util.project" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
