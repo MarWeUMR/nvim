e9ef6feee27935e27b3e57c311333898878edb4c
Marcus Weber <weberma@students.uni-marburg.de>
2023-05-07 20:35:49 +0200

    reactivate mini-animate, given the perf. improvement with current neovim master PR

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,12 +12,12 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     {
       "LazyVim/LazyVim",
-      import = "lazyvim.plugins"
+      import = "lazyvim.plugins",
     },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.util.project" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
