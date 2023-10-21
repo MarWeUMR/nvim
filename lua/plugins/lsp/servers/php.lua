2956bb80f5ee40c9f3f50d8bffa09f51b4e43a71
Marcus Weber <marcus.wallau@gmail.com>
2023-10-21 12:54:22 +0200

    update: remove unnecessary none-ls usage

--- lua/plugins/lsp/servers/php.lua
+++ lua/plugins/lsp/servers/php.lua
@@ -17,16 +17,4 @@ return {
     },
     setup = {},
   },
-
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      if type(opts.sources) == "table" then
-        local nls = require("null-ls")
-        vim.list_extend(opts.sources, {
-          nls.builtins.formatting.phpcsfixer,
-        })
-      end
-    end,
-  },
 }
