254a619a6d7a58cbc2cb362d4f64b9ff9fe869ba
Marcus Weber <marcus.wallau@gmail.com>
2023-10-21 12:54:38 +0200

    feat: add lazyvim extras black and prettier

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,8 +22,10 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
   },
