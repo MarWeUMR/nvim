2eb03036f559b53b61b183237429034ab6b5839f
Marcus Weber <marcus.wallau@gmail.com>
2024-03-14 21:29:46 +0100

    feat: add a few more plugins

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -4,4 +4,34 @@ return {
     "nvimtools/hydra.nvim",
     event = "VeryLazy",
   },
+  {
+    "echasnovski/mini.move",
+    opts = {
+
+      mappings = {
+        -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+        left = "<M-H>",
+        right = "<M-L>",
+        down = "<M-J>",
+        up = "<M-K>",
+
+        -- Move current line in Normal mode
+        line_left = "<M-H>",
+        line_right = "<M-L>",
+        line_down = "<M-J>",
+        line_up = "<M-K>",
+      },
+
+      -- Options which control moving behavior
+      options = {
+        -- Automatically reindent selection during linewise vertical move
+        reindent_linewise = true,
+      },
+    },
+  },
+  {
+    "dgagn/diagflow.nvim",
+    event = "LspAttach",
+    opts = {},
+  },
 }
