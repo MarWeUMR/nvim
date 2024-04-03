afc00851404e73658cc61c8c669b114912489dcb
Marcus Weber <marcus.wallau@gmail.com>
2024-04-03 15:45:14 +0200

    feat: use nightly neogit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,7 @@
 return {
   {
     "NeogitOrg/neogit",
+    branch = "nightly",
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
       "sindrets/diffview.nvim", -- optional - Diff integration
