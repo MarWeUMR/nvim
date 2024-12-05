de2e0cbd77aa7c81fec0e08192c34fed10db3b1a
Marcus Weber <marwe@marcuss-mini.speedport.ip>
2024-12-05 10:16:19 +0100

    chore: turn on gitsigns line blame

---
+++ lua/plugins/git.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "right_align",
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
+    },
+  },
+}
