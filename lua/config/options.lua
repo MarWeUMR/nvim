434530aa30e47b2a942a4a3396cf74c411960c32
Marcus Weber <marwe@marcuss-mini.speedport.ip>
2024-12-15 21:33:55 +0100

    chore: improve options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,5 +6,21 @@ vim.filetype.add({
   },
 })
 
+vim.diagnostic.config({
+  -- add the source of the diagnostic to the displayed message
+  -- float = {
+  --   source = "always",
+  -- },
+
+  -- for tiny inline diagnostics
+  virtual_text = false,
+})
+
 vim.o.pumblend = 0
 vim.o.scrolloff = 8
+
+-- vim.o.spelllang = "en_us,de_de"
+vim.o.spelllang = ""
+
+vim.g.ai_cmp = false
+-- vim.g.lazyvim_php_lsp = "intelephense"
