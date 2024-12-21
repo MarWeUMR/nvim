0f69b594b3507c86d65927c792de16fae8329a4e
Marcus Weber <marwe@marcuss-mini.speedport.ip>
2024-12-21 10:40:55 +0100

    feat: add yazi.nvim

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>y",
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>Y",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+      {
+        -- NOTE: this requires a version of yazi that includes
+        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+        "<c-y>",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
+    },
+  },
+}
